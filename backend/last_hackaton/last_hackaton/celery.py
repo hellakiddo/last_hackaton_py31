81e3734c1e80f9a86206311c0a83ec82601c22f5
A.M <113894011+Amir800S@users.noreply.github.com>
2024-02-07 01:21:25 +0300

    registration, auto-deploy, fixed groups, groups_subscriptions, n+1 added in Group

--- backend/last_hackaton/last_hackaton/celery.py
+++ backend/last_hackaton/last_hackaton/celery.py
@@ -6,7 +6,9 @@ os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'last_hackaton.settings'
 )
 
-app = Celery('last_hackaton')
+app = Celery(
+    'last_hackaton', broker='redis://158.160.9.246:6379/0',backend='redis://158.160.9.246:6379/0',
+)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
