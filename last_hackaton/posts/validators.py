b676a8e5f24db3727d813bd7e041a984bcb58757
Mash556 <almashnurmatk.gmail.com>
2024-02-11 15:52:06 +0600

    .

--- last_hackaton/posts/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.validators import RegexValidator
-
-name_validator = RegexValidator(
-    regex=r'^#[^\s]+$',
-    message='Хэштэг должен начинаться с "#" и не содержать пробелов.',
-)
